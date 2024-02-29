b924a2968f8dfe79a72da6ab3d80fd1bc1b8795c
piercebhunt <huntpie@gmail.com>
2024-02-28 19:50:08 -0500

    chapter 2

--- simple.cpp
+++ simple.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
 int main()
 {
+    std::cout << "Enter two numbers:" << std::end1;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2
+              << " is " << v1 _ v2 << std::end1;
     return 0;
+
 }
